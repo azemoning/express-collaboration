6094b01b1a4fc2843e13b4e2d395fc7ca508e99b
Amir <amiruladlilhakim@gmail.com>
2021-01-29 17:30:32 +0700

    edit rootRoute.js

--- route/rootRoute.js
+++ route/rootRoute.js
@@ -1,5 +1,22 @@
 const express = require('express')
+var faker = require('faker')
 const app = express.Router()
+
+var randomNewsTitle = [] //array yang berisi judul-judul berita 
+var randomNews = [] //array yang berisi paragraf-paragraf berita
+var randomImages = [] //berisi link gambar-gambar kucing
+
+for (var i = 0; i < 20; i++) {
+    randomNewsTitle[i] = faker.lorem.sentence()
+}
+
+for (var i = 0; i < 20; i++) {
+    randomNews[i] = faker.lorem.paragraphs()
+}
+
+for (var i = 0; i < 20; i++) {
+    randomImages[i] = faker.image.cats()
+}
 
 let counter = 1
 
@@ -7,6 +24,8 @@ app.get('/', (req, res) => {
     counter++
     res.render('index', {
         counter: counter,
+        titles: randomNewsTitle,
+        images: randomImages
     })
 })
 
@@ -21,6 +40,9 @@ app.get('/', (req, res) => {
     counter++
     res.render('news', {
         counter: counter,
+        titles: randomNewsTitle,
+        images: randomImages,
+        news: randomNews
     })
 })
 
