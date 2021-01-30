41cee6285316bdcb480ef26fd7a4f65f2ddd6ea3
azemoning <ahmadsaufimaulana@gmail.com>
2021-01-30 22:49:58 +0700

    implement dynamic news

--- route/rootRoute.js
+++ route/rootRoute.js
@@ -2,52 +2,38 @@ const express = require('express')
 var faker = require('faker')
 const app = express.Router()
 
-var randomNewsTitle = [] //array yang berisi judul-judul berita 
-var randomNews = [] //array yang berisi paragraf-paragraf berita
-var randomImages = [] //berisi link gambar-gambar kucing, biar unik hehe
+let newsContent = []
 
-for (var i = 0; i < 20; i++) {
-    randomNewsTitle[i] = faker.lorem.sentence()
+for (var i = 0; i < 5; i++) {
+    newsContent.push({
+        index: i,
+        title: faker.random.words(),
+        news: faker.lorem.paragraphs(),
+        image: faker.image.people()
+    })
 }
 
-for (var i = 0; i < 20; i++) {
-    randomNews[i] = faker.lorem.paragraphs()
-}
-
-for (var i = 0; i < 20; i++) {
-    randomImages[i] = faker.image.cats()
-}
-
-let counter = 1
+let counter = 0
 
 app.get('/', (req, res) => {
     counter++
     res.render('index', {
         counter: counter,
-        titles: randomNewsTitle,
-        images: randomImages
+        newsContent: newsContent
     })
 })
 
-app.get('/', (req, res) => {
-    counter++
-    res.render('about', {
-        counter: counter,
-    })
-})
-
-app.get('/', (req, res) => {
-    counter++
+app.get('/news/:id', (req, res) => {
     res.render('news', {
-        counter: counter,
-        titles: randomNewsTitle,
-        images: randomImages,
-        news: randomNews
+        newsContent: newsContent[req.params.id]
     })
 })
 
 app.get('/:path', (req, res) => {
-    res.render(req.params.path, {}, (err, html) => {
+    res.render(req.params.path, {
+        counter: counter,
+        newsContent: newsContent
+    }, (err, html) => {
         if (err) {
             res.render('404')
         } else {
